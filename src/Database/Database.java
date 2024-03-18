d1268bf12271d218192cb8e5f4629e8c8c19f8fd
trainadavide <davide.traina@edu.unifi.it>
2024-03-18 13:39:24 +0100

    database implemented

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -1,12 +1,50 @@
 package Database;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Database {
-    public static void main(String[] args) {
+    private static final String URL = "jdbc:postgresql://localhost:5432/BoardgameManager";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "root";
 
+    public static void main(String[] args) {
+        Connection connection = null;
+
+        try {
+            // Carica il driver JDBC per PostgreSQL
+            Class.forName("org.postgresql.Driver");
+
+            // Effettua la connessione al database PostgreSQL
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            // Se la connessione ha avuto successo
+            if (connection != null) {
+                System.out.println("Connessione al database stabilita!");
+                // Crea uno statement per eseguire la query
+                Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                //QUERY
+                ResultSet resultSet = statement.executeQuery("Select * from boardgame");
+                resultSet.absolute(1);
+                System.out.println(resultSet.getString("name"));
+            } else {
+                System.out.println("Connessione al database non riuscita!");
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver JDBC non trovato!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Errore durante la connessione al database!");
+            e.printStackTrace();
+        } finally {
+            // Chiudi la connessione quando hai finito
+            try {
+                if (connection != null) {
+                    connection.close();
+                    System.out.println("Connessione al database chiusa!");
+                }
+            } catch (SQLException e) {
+                System.out.println("Errore durante la chiusura della connessione!");
+                e.printStackTrace();
+            }
+        }
     }
 }
