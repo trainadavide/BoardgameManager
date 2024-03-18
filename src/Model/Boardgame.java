bc0607b7b3d77ee8605be52e6047bbbdbb6b9ec8
trainadavide <davide.traina@edu.unifi.it>
2024-03-18 09:04:42 +0100

    Boardgame class updated

--- src/Model/Boardgame.java
+++ src/Model/Boardgame.java
@@ -6,13 +6,16 @@ public class Boardgame {
     private int minPlayers;
     private int maxPlayers;
     private int avgGameDuration;
+    private boolean competitive;
 
-    public Boardgame(int id, String name, int minPlayers, int maxPlayers, int avgGameDuration) {
+    public Boardgame(int id, String name, int minPlayers, int maxPlayers, int avgGameDuration, boolean competitive) {
         this.id = id;
         this.name = name;
         this.minPlayers = minPlayers;
         this.maxPlayers = maxPlayers;
+        this.competitive = competitive;
         this.avgGameDuration = avgGameDuration;
+
     }
 
     public int getMaxPlayers() {
